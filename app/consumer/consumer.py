336bcda93f2b67a207397fcbb1f6f6c7e0142fc1
Amit Singh <amit.singh@PNQM2026-AMSING.local>
2026-04-21 02:00:05 +0530

    fix consumer bug

--- app/consumer/consumer.py
+++ app/consumer/consumer.py
@@ -1,17 +1,14 @@
-from kafka import KafkaConsumer
-import ssl
-import json
 import os
+import json
+from kafka import KafkaConsumer
 
 kafka_broker = os.environ.get("KAFKA_BROKER_URL")
 kafka_topic = os.environ.get("KAFKA_TOPIC", "posts")
 
-context = ssl.create_default_context()
-
-cconsumer = KafkaConsumer(
+consumer = KafkaConsumer(
     kafka_topic,
     bootstrap_servers=kafka_broker.split(","),
-    security_protocol="SSL",   # 👈 REQUIRED
+    security_protocol="SSL",
     auto_offset_reset="earliest",
     value_deserializer=lambda m: json.loads(m.decode("utf-8")),
 )
@@ -19,4 +16,4 @@ cconsumer = KafkaConsumer(
 print("Listening...")
 
 for msg in consumer:
-    print(msg.value)
+    print(f"Received message: {msg.value}")
